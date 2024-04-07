cea3dca7fcfa7f530d9e6544d7bef7aa87edd028
Artur <arturvasylkivsky@gmail.com>
2024-04-07 10:15:59 +0300

    cvbnm

--- main.cpp
+++ main.cpp
@@ -1,40 +1,23 @@
 #include <iostream>
 
 class Point {
-    int x = 0;
-    int y = 0;
+    int x;
+    int y;
 
 public:
-    Point() {
+    Point() : x(0), y(0) {
         std::cout << "x: " << x << " Point Default constructor\n";
     }
-    Point(int pX, int pY) {
-        x = pX;
-        y = pY;
+    Point(int pX, int pY) : x(pX), y(pY) {
         std::cout << "Point Parametrized constructor\n";
     }
-    Point(const Point& other) {
-        x = other.x;
-        y = other.y;
+    Point(const Point& other) : x(other.x), y(other.y) {
         std::cout << "Point Copy constructor\n";
-    }
-
-    ~Point() {
-        std::cout << "Point Destructor\n";
-    }
-
-    int getX() const {
-        return x;
-    }
-
-    int getY() const {
-        return y;
     }
 };
 
 int main() {
     Point p1(3, 4); 
-    Point p2(p1);
-    std::cout << "p2.x: " << p2.getX() << ", p2.y: " << p2.getY() << std::endl;
+    Point p2(p1);   
     return 0;
 }
